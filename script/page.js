79d05caa36badc04a1c5111b9553780c39883765
jpemberthy <jpemberthy@gmail.com>
2011-12-08 22:08:20 -0500

    Disables the slider on /about and adds a pic of me zomg.

--- script/page.js
+++ script/page.js
@@ -1,40 +1,38 @@
 
 	$('.fancybox').fancybox({});
-	
+
 	$('.fancybox img').captify();
-	
+
 	$('.gallery-list').hover(
 		function() {},
 		function()
 		{
 			$(this).find('li img').animate({opacity:1},250);
-		}	
+		}
 	);
-	
+
 	$('.gallery-list li').hover(
-		function() 
+		function()
 		{
 			$(this).siblings('li').find('img').css('opacity',0.5);
 			$(this).find('img').animate({opacity:1},250);
 		},
 		function()
 		{
-			$(this).find('img').css('opacity',1);	
+			$(this).find('img').css('opacity',1);
 		}
 	);
-	
-	$('#slider').nivoSlider({directionNav:false});
-	
+
 	try
 	{
 		var coordinate=new google.maps.LatLng(29.760193,-95.36939);
-	   var mapOptions= 
+	   var mapOptions=
 		{
 			zoom:10,
 			center:coordinate,
 			mapTypeId:google.maps.MapTypeId.ROADMAP
 	   };
-										
+
 		var googleMap=new google.maps.Map(document.getElementById('map'),mapOptions);
 	}
 	catch(e) {}
