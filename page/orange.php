dde5c11b40bfb2e1188c0057d0414fff12bd3a12
jpemberthy <jpemberthy@gmail.com>
2011-12-09 16:06:11 -0500

    on it ...

--- page/orange.php
+++ page/orange.php
@@ -1,26 +1,27 @@
 <div class="layout-50 clear-fix overflow-fix">
   <div class="layout-50-left">
+    working on it ...
 
-  	<h3>On The Map</h3>
-  	<div id="map"></div>
+    <!-- <h3>On The Map</h3>
+    <div id="map"></div>
 
-  	<br/>
+    <br/>
 
-  	<h3>Anna Brown Studio</h3>
-  	<p>
-  		1370 Northwood, 15<br/>
-  		Houston, TX72150, United States Of America
-  	</p>
+    <h3>Anna Brown Studio</h3>
+    <p>
+      1370 Northwood, 15<br/>
+      Houston, TX72150, United States Of America
+    </p>
 
-  	<ul class="no-list contact-list">
-  		<li class="contact-list-phone"><span>Phone:</span> 1.800.353.252</li>
-  		<li class="contact-list-fax"><span>Fax:</span> 1.800.353.253</li>
-  		<li class="contact-list-mail"><span>Mail:</span> anna.brown@mail.com</li>
-  	</ul>
+    <ul class="no-list contact-list">
+      <li class="contact-list-phone"><span>Phone:</span> 1.800.353.252</li>
+      <li class="contact-list-fax"><span>Fax:</span> 1.800.353.253</li>
+      <li class="contact-list-mail"><span>Mail:</span> anna.brown@mail.com</li>
+    </ul>
 
   </div>
 
   <div class="layout-50-right">
-  	<h3>Tocame gordi</h3>
-  </div>
+    <h3>Tocame gordi</h3>
+  </div> -->
 </div>
