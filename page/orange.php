4b6c9b4f70b96145c9d0f58e9b00d26667f80ddf
jpemberthy <jpemberthy@gmail.com>
2011-12-09 16:35:27 -0500

    country music.

--- page/orange.php
+++ page/orange.php
@@ -1,27 +1,8 @@
 <div class="layout-50 clear-fix overflow-fix">
-  <div class="layout-50-left">
-    working on it ...
-
-    <!-- <h3>On The Map</h3>
-    <div id="map"></div>
-
-    <br/>
-
-    <h3>Anna Brown Studio</h3>
-    <p>
-      1370 Northwood, 15<br/>
-      Houston, TX72150, United States Of America
-    </p>
-
-    <ul class="no-list contact-list">
-      <li class="contact-list-phone"><span>Phone:</span> 1.800.353.252</li>
-      <li class="contact-list-fax"><span>Fax:</span> 1.800.353.253</li>
-      <li class="contact-list-mail"><span>Mail:</span> anna.brown@mail.com</li>
-    </ul>
-
+  <div>
+    <h3>I forgot to say love Country Music</h3>
+    
+    <br/><br/>
+    <iframe width="420" height="315" src="http://www.youtube.com/embed/-Fulz4ytZ54" frameborder="0" allowfullscreen></iframe>
   </div>
-
-  <div class="layout-50-right">
-    <h3>Tocame gordi</h3>
-  </div> -->
 </div>
--- script/page.js
+++ script/page.js
@@ -22,17 +22,3 @@
 			$(this).find('img').css('opacity',1);
 		}
 	);
-
-	try
-	{
-		var coordinate=new google.maps.LatLng(29.760193,-95.36939);
-	   var mapOptions=
-		{
-			zoom:10,
-			center:coordinate,
-			mapTypeId:google.maps.MapTypeId.ROADMAP
-	   };
-
-		var googleMap=new google.maps.Map(document.getElementById('map'),mapOptions);
-	}
-	catch(e) {}
