3f2414297690938faaec295db0ee7e6c8eb94c10
jpemberthy <jpemberthy@gmail.com>
2011-12-12 19:32:44 -0500

    time hub and orange update.

--- script/page.js
+++ script/page.js
@@ -1,24 +1,25 @@
+$('.fancybox').fancybox({
+  'titlePosition': "over"
+});
 
-	$('.fancybox').fancybox({});
+$('.fancybox img').captify();
 
-	$('.fancybox img').captify();
+$('.gallery-list').hover(
+ function() {},
+ function()
+ {
+   $(this).find('li img').animate({opacity:1},250);
+ }
+);
 
-	$('.gallery-list').hover(
-		function() {},
-		function()
-		{
-			$(this).find('li img').animate({opacity:1},250);
-		}
-	);
-
-	$('.gallery-list li').hover(
-		function()
-		{
-			$(this).siblings('li').find('img').css('opacity',0.5);
-			$(this).find('img').animate({opacity:1},250);
-		},
-		function()
-		{
-			$(this).find('img').css('opacity',1);
-		}
-	);
+$('.gallery-list li').hover(
+ function()
+ {
+   $(this).siblings('li').find('img').css('opacity',0.5);
+   $(this).find('img').animate({opacity:1},250);
+ },
+ function()
+ {
+   $(this).find('img').css('opacity',1);
+ }
+);
