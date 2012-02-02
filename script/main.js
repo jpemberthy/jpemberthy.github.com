384f133ec5cc33568d34f55214eacbff74b7f685
jpemberthy <jpemberthy@gmail.com>
2012-02-01 22:18:04 -0500

    Fixes portfolio reference and adds popup.

--- script/main.js
+++ script/main.js
@@ -1,31 +1,29 @@
+$(document).ready(function(){
+ $('.cascade').casadeLanding();
 
-	$(document).ready(function()
-	{
-		$('.cascade').casadeLanding();
+ $.getJSON('http://twitter.com/statuses/user_timeline.json?screen_name=jpemberthy&count=5&callback=?',
+  function(data) {
+   if(data.length)
+   {
+     var list=$('<ul>');
+     $(data).each(function(index,value)
+     {
+       list.append($('<li>').append($('<p>').html(linkify(value.text))));
+     });
 
-		$.getJSON('http://twitter.com/statuses/user_timeline.json?screen_name=jpemberthy&count=5&callback=?', function(data)
-		{
-			if(data.length)
-			{
-				var list=$('<ul>');
-				$(data).each(function(index,value)
-				{
-					list.append($('<li>').append($('<p>').html(linkify(value.text))));
-				});
+     $('#latest-tweets').append(list);
 
-				$('#latest-tweets').append(list);
+     list.bxSlider(
+     {
+       auto: true,
+       mode:'vertical',
+       nextText:null,
+       prevText:null,
+       displaySlideQty:1,
+        pause:5000
+     });
 
-				list.bxSlider(
-				{
-					auto: true,
-					mode:'vertical',
-					nextText:null,
-					prevText:null,
-					displaySlideQty:1,
-				   pause:5000
-				});
-
-				$('#latest-tweets a').attr('target','_blank');
-			}
-		});
-	});
+     $('#latest-tweets a').attr('target','_blank');
+   }
+ });
+});
